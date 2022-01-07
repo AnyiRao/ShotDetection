c47fee80f8b53e891b00794162ad5c613f5ab46c
ray117 <anyirao@link.cuhk.edu.hk>
2022-01-07 20:05:36 +0800

    first commit

---
+++ shotdetect/platform.py
@@ -0,0 +1,151 @@
+# The codes below partially refer to the PySceneDetect. According
+# to its BSD 3-Clause License, we keep the following.
+#
+#          PySceneDetect: Python-Based Video Scene Detector
+#   ---------------------------------------------------------------
+#     [  Site: http://www.bcastell.com/projects/PySceneDetect/   ]
+#     [  Github: https://github.com/Breakthrough/PySceneDetect/  ]
+#     [  Documentation: http://pyscenedetect.readthedocs.org/    ]
+#
+# Copyright (C) 2014-2021 Brandon Castellano <http://www.bcastell.com>.
+
+"""
+This file contains all platform/library/OS-specific compatibility fixes,
+intended to improve the systems that are able to run ShotDetect, and allow
+for maintaining backwards compatibility with existing libraries going forwards.
+Other helper functions related to the detection of the appropriate dependency
+DLLs on Windows and getting uniform line-terminating csv reader/writer objects
+are also included in this module.
+
+With respect to the Python standard library itself and Python 2 versus 3,
+this module adds compatibility wrappers for Python's Queue/queue (Python 2/3,
+respectively) as scenedetect.platform.queue.
+
+For OpenCV 2.x, the scenedetect.platform module also makes a copy of the
+OpenCV VideoCapture property constants from the cv2.cv namespace directly
+to the cv2 namespace.  This ensures that the cv2 API is consistent
+with those changes made to it in OpenCV 3.0 and above.
+
+This module also includes an alias for the unicode/string types in Python 2/3
+as STRING_TYPE intended to help with parsing string types from the CLI parser.
+"""
+
+from __future__ import print_function
+import sys
+import os
+import platform
+import struct
+import csv
+import cv2
+
+# pylint: disable=unused-import
+
+
+##
+## Python 2/3 Queue/queue Library (scenedetect.platform.queue)
+##
+
+if sys.version_info[0] == 2:
+    import Queue as queue
+else:
+    import queue
+
+try:
+    from tqdm import tqdm
+except ImportError:
+    tqdm = None
+
+
+# pylint: enable=unused-import
+
+# String type (used to allow FrameTimecode object to take both unicode and native
+# string objects when being constructed via scenedetect.platform.STRING_TYPE).
+# pylint: disable=invalid-name, undefined-variable
+if sys.version_info[0] == 2:
+    STRING_TYPE = unicode
+else:
+    STRING_TYPE = str
+# pylint: enable=invalid-name, undefined-variable
+
+
+##
+## OpenCV 2.x Compatibility Fix
+##
+
+# Compatibility fix for OpenCV v2.x (copies CAP_PROP_* properties from the
+# cv2.cv namespace to the cv2 namespace, as the cv2.cv namespace was removed
+# with the release of OpenCV 3.0).
+# pylint: disable=c-extension-no-member
+if cv2.__version__[0] == '2' or not (
+        cv2.__version__[0].isdigit() and int(cv2.__version__[0]) >= 3):
+    cv2.CAP_PROP_FRAME_WIDTH = cv2.cv.CV_CAP_PROP_FRAME_WIDTH
+    cv2.CAP_PROP_FRAME_HEIGHT = cv2.cv.CV_CAP_PROP_FRAME_HEIGHT
+    cv2.CAP_PROP_FPS = cv2.cv.CV_CAP_PROP_FPS
+    cv2.CAP_PROP_POS_MSEC = cv2.cv.CV_CAP_PROP_POS_MSEC
+    cv2.CAP_PROP_POS_FRAMES = cv2.cv.CV_CAP_PROP_POS_FRAMES
+    cv2.CAP_PROP_FRAME_COUNT = cv2.cv.CV_CAP_PROP_FRAME_COUNT
+# pylint: enable=c-extension-no-member
+
+
+def check_opencv_ffmpeg_dll():
+    """ Check OpenCV FFmpeg DLL: Checks if OpenCV video I/O support is available,
+    on Windows only, by checking for the appropriate opencv_ffmpeg*.dll file.
+
+    On non-Windows systems always returns True, or for OpenCV versions that do
+    not follow the X.Y.Z version numbering pattern. Thus there may be false
+    positives (True) with this function, but not false negatives (False).
+    In those cases, ShotDetect will report that it could not open the
+    video file, and for Windows users, also gives an additional warning message
+    that the error may be due to the missing DLL file.
+
+    Returns:
+        (bool) True if OpenCV video support is detected (e.g. the appropriate
+        opencv_ffmpegXYZ.dll file is in PATH), False otherwise.
+    """
+    if platform.system() == 'Windows' and (
+            cv2.__version__[0].isdigit() and cv2.__version__.find('.') > 0):
+        is_64_bit_str = '_64' if struct.calcsize("P") == 8 else ''
+        dll_filename = 'opencv_ffmpeg{OPENCV_VERSION}{IS_64_BIT}.dll'.format(
+            OPENCV_VERSION=cv2.__version__.replace('.', ''),
+            IS_64_BIT=is_64_bit_str)
+        return any([os.path.exists(os.path.join(path_path, dll_filename))
+                    for path_path in os.environ['PATH'].split(';')]), dll_filename
+    return True
+
+
+def _get_cv2_param(param_name):
+    if param_name.startswith('CV_'):
+        param_name = param_name[3:]
+    try:
+        return getattr(cv2, param_name)
+    except AttributeError:
+        return None
+
+
+def get_cv2_imwrite_params():
+    """ Get OpenCV imwrite Params: Returns a dict of supported image formats and
+    their associated quality/compression parameter.
+
+    Returns:
+        (Dict[str, int]) Dictionary of image formats/extensions ('jpg',
+            'png', etc...) mapped to the respective OpenCV quality or
+            compression parameter (e.g. 'jpg' -> cv2.IMWRITE_JPEG_QUALITY,
+            'png' -> cv2.IMWRITE_PNG_COMPRESSION)..
+    """
+    return {
+        'jpg': _get_cv2_param('IMWRITE_JPEG_QUALITY'),
+        'png': _get_cv2_param('IMWRITE_PNG_COMPRESSION'),
+        'webp': _get_cv2_param('IMWRITE_WEBP_QUALITY')
+    }
+
+
+def get_csv_reader(file_handle):
+    # type: (File) -> csv.reader
+    """ Returns a csv.reader object using the passed file handle. """
+    return csv.reader(file_handle, lineterminator='\n')
+
+
+def get_csv_writer(file_handle):
+    # type: (File) -> csv.writer
+    """ Returns a csv.writer object using the passed file handle. """
+    return csv.writer(file_handle, lineterminator='\n')
---
+++ shotdetect/video_splitter.py
@@ -0,0 +1,189 @@
+# The codes below partially refer to the PySceneDetect. According
+# to its BSD 3-Clause License, we keep the following.
+#
+#          PySceneDetect: Python-Based Video Scene Detector
+#     -----------------------------------------------------------
+#     [  Site: http://www.bcastell.com/projects/PySceneDetect/   ]
+#     [  Github: https://github.com/Breakthrough/PySceneDetect/  ]
+#     [  Documentation: http://pyscenedetect.readthedocs.org/    ]
+#
+# Copyright (C) 2014-2021 Brandon Castellano <http://www.bcastell.com>.
+
+import logging
+import math
+import os
+import pdb
+import subprocess
+import time
+from string import Template
+
+from shotdetect.platform import tqdm
+
+
+def is_mkvmerge_available():
+    """ Is mkvmerge Available: Gracefully checks if mkvmerge command is available.
+
+    Returns:
+        (bool) True if the mkvmerge command is available, False otherwise.
+    """
+    ret_val = None
+    try:
+        ret_val = subprocess.call(['mkvmerge', '--quiet'])
+    except OSError:
+        return False
+    if ret_val is not None and ret_val != 2:
+        return False
+    return True
+
+
+def is_ffmpeg_available():
+    """ Is ffmpeg Available: Gracefully checks if ffmpeg command is available.
+
+    Returns:
+        (bool) True if the ffmpeg command is available, False otherwise.
+    """
+    ret_val = None
+    try:
+        ret_val = subprocess.call(['ffmpeg', '-v', 'quiet'])
+    except OSError:
+        return False
+    if ret_val is not None and ret_val != 1:
+        return False
+    return True
+
+
+def split_video_mkvmerge(input_video_paths, shot_list, output_file_prefix,
+                         video_name, suppress_output=False):
+    """ Calls the mkvmerge command on the input video(s), splitting it at the
+    passed timecodes, where each shot is written in sequence from 001. """
+
+    if not input_video_paths or not shot_list:
+        return
+
+    logging.info('Splitting input video%s using mkvmerge, output path template:\n  %s',
+            's' if len(input_video_paths) > 1 else '', output_file_prefix)
+
+    ret_val = None
+    # mkvmerge automatically appends '-$SHOT_NUMBER'.
+    output_file_name = output_file_prefix.replace('-${SHOT_NUMBER}', '')
+    output_file_name = output_file_prefix.replace('-$SHOT_NUMBER', '')
+    output_file_template = Template(output_file_name)
+    output_file_name = output_file_template.safe_substitute(
+        VIDEO_NAME=video_name,
+        SHOT_NUMBER='')
+
+    try:
+        call_list = ['mkvmerge']
+        if suppress_output:
+            call_list.append('--quiet')
+        call_list += [
+            '-o', output_file_name,
+            '--split',
+            #'timecodes:%s' % ','.join(
+            #    [start_time.get_timecode() for start_time, _ in shot_list[1:]]),
+            'parts:%s' % ','.join(
+                ['%s-%s' % (start_time.get_timecode(), end_time.get_timecode())
+                 for start_time, end_time in shot_list]),
+            ' +'.join(input_video_paths)]
+        total_frames = shot_list[-1][1].get_frames() - shot_list[0][0].get_frames()
+        processing_start_time = time.time()
+        ret_val = subprocess.call(call_list)
+        if not suppress_output:
+            print('')
+            logging.info('Average processing speed %.2f frames/sec.',
+                         float(total_frames) / (time.time() - processing_start_time))
+    except OSError:
+        logging.error('mkvmerge could not be found on the system.'
+                      ' Please install mkvmerge to enable video output support.')
+        raise
+    if ret_val is not None and ret_val != 0:
+        logging.error('Error splitting video (mkvmerge returned %d).', ret_val)
+
+
+def split_video_ffmpeg(input_video_paths, shot_list, output_dir,
+                       output_file_template="${OUTPUT_DIR}/shot_${SHOT_NUMBER}.mp4",
+                       compress_output=False,
+                       hide_progress=False,
+                       suppress_output=False):
+    """ Calls the ffmpeg command on the input video(s), generating a new video for
+    each shot based on the start/end timecodes. """
+
+    os.makedirs(output_dir, exist_ok=True)
+    if not input_video_paths or not shot_list:
+        return
+
+    logging.info(
+        'Splitting input video%s using ffmpeg, output path template:\n  %s',
+        's' if len(input_video_paths) > 1 else '', output_file_template)
+    if len(input_video_paths) > 1:
+        # TODO: Add support for splitting multiple/appended input videos.
+        # https://trac.ffmpeg.org/wiki/Concatenate#samecodec
+        # Requires generating a temporary file list for ffmpeg.
+        logging.error(
+            'Sorry, splitting multiple appended/concatenated input videos with'
+            ' ffmpeg is not supported yet. This feature will be added to a future'
+            ' version of ShotDetect. In the meantime, you can try using the'
+            ' -c / --copy option with the split-video to use mkvmerge, which'
+            ' generates less accurate output, but supports multiple input videos.')
+        raise NotImplementedError()
+
+    ret_val = None
+    filename_template = Template(output_file_template)
+    shot_num_format = '%0'
+    shot_num_format += str(max(4, math.floor(math.log(len(shot_list), 10)) + 1)) + 'd'
+    try:
+        progress_bar = None
+        total_frames = shot_list[-1][1].get_frames() - shot_list[0][0].get_frames()
+        if tqdm and not hide_progress:
+            progress_bar = tqdm(total=total_frames, unit='frame', miniters=1, desc="Split Video")
+        processing_start_time = time.time()
+        for i, (start_time, end_time) in enumerate(shot_list):
+            end_time = end_time.__sub__(1)  # Fix the last frame of a shot to be 1 less than the first frame of the next shot
+            duration = (end_time - start_time)
+            # an alternative way to do it
+            # duration = (end_time.get_frames()-1)/end_time.framerate - (start_time.get_frames())/start_time.framerate
+            # duration_frame = end_time.get_frames()-1 - start_time.get_frames()
+            call_list = ['ffmpeg']
+            if suppress_output:
+                call_list += ['-v', 'quiet']
+            elif i > 0:
+                # Only show ffmpeg output for the first call, which will display any
+                # errors if it fails, and then break the loop. We only show error messages
+                # for the remaining calls.
+                call_list += ['-v', 'error']
+            call_list += [
+                '-y',
+                '-ss',
+                start_time.get_timecode(),
+                '-i',
+                input_video_paths[0]]
+            if compress_output:
+                call_list += '[-crf 21]'  # compress
+            call_list += ['-map_chapters', '-1']  # remove meta stream
+            call_list += [
+                '-strict',
+                '-2',
+                '-t',
+                duration.get_timecode(),
+                '-sn',
+                filename_template.safe_substitute(
+                    OUTPUT_DIR=output_dir,
+                    SHOT_NUMBER=shot_num_format % (i))
+                ]
+            ret_val = subprocess.call(call_list)
+            if not suppress_output and i == 0 and len(shot_list) > 1:
+                logging.info(
+                    'Output from ffmpeg for shot 1 shown above, splitting remaining shots...')
+            if ret_val != 0:
+                break
+            if progress_bar:
+                progress_bar.update(duration.get_frames()+1)  # to compensate the missing one frame caused above
+        if progress_bar:
+            print('')
+            logging.info('Average processing speed %.2f frames/sec.',
+                         float(total_frames) / (time.time() - processing_start_time))
+    except OSError:
+        logging.error('ffmpeg could not be found on the system.'
+                      ' Please install ffmpeg to enable video output support.')
+    if ret_val is not None and ret_val != 0:
+        logging.error('Error splitting video (ffmpeg returned %d).', ret_val)
